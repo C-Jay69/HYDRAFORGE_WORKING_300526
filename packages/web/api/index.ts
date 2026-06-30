530f5a71bc8f67a4eae4dcd9d96b949e2e6c8a83
c-jay69 <simonjc.mex@gmail.com>
2026-06-30 14:03:02 -0600

    fixes

--- packages/web/api/index.ts
+++ packages/web/api/index.ts
@@ -1,9 +1,2 @@
-export default async function handler(request) {
-  return new Response(JSON.stringify({ 
-    message: "THE BRIDGE IS WORKING!", 
-    timestamp: new Date().toISOString(),
-    status: "SUCCESS" 
-  }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
+import app from "../src/index";
+export default app;
